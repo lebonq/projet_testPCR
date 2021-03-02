834ad04ccd5523d737d35f554fa05e0e1cc9b5e3
lebonq <lebonquentin@live.fr>
2021-03-02 08:46:57 +0100

    suppression dossier lib

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello World\n");
-    int a = 2;
-    int b = 2;
-    int c = a + b;
-    printf("%d\n",c);
-}
---
+++ terminal.c
@@ -0,0 +1,6 @@
+#include "terminal.h"
+#include "message.h"
+
+int main(){
+    
+}
